953c124fd4b4ae6772e2f65c76c118af0612789e
Guilherme dos Santos <guilherme@guisantos.com.br>
2020-08-29 01:04:04 -0300

    BodyParser

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core'
+import bodyParser from 'body-parser'
 import { AppModule } from './app.module'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+  app.use(bodyParser.urlencoded({ extended: true }))
+  app.use(bodyParser.json())
+  app.setGlobalPrefix('api/v1')
   await app.listen(process.env.PORT)
 } 
 
