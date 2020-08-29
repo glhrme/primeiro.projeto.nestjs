e6110b58d9af6ff83538a394e99ccc7144d4c351
Guilherme dos Santos <guilherme@guisantos.com.br>
2020-08-29 04:38:20 -0300

    Configura database

---
+++ src/database/database.provider.ts
@@ -0,0 +1,25 @@
+import { createConnection } from 'typeorm'
+
+export const databaseProvider = [
+  {
+    provide: 'DATABASE_CONNECTION',
+    useFactory: async () => await createConnection({
+      type: 'mysql',
+      host: process.env.MYSQL_HOST,
+      port: parseInt(process.env.MYSQL_HOST, 10),
+      username: process.env.MYSQL_USERNAME,
+      password: process.env.MYSQL_PASSWORD,
+      database: process.env.MYSQL_DATABASE,
+      entities: [
+        __dirname + '/../**/*.entity{.ts,.js}'
+      ],
+      migrations: [
+        "./migration/**/*.ts"
+      ],
+      subscribers: [
+        "../subscriber/**/*.ts"
+      ],
+      synchronize: true
+    })
+  }
+]
