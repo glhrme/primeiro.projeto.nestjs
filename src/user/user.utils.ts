18bf54eaed5e45106e25dec5c3a70414eaa27021
Guilherme dos Santos <guilherme@guisantos.com.br>
2020-08-29 18:22:17 -0300

    Adiciona método de validação de usuário

---
+++ src/user/user.utils.ts
@@ -0,0 +1,18 @@
+import { validate } from 'class-validator'
+import IUser from './interfaces/user.interface'
+import { User } from 'entities/user.entity'
+import { BadRequestException } from '@nestjs/common'
+
+export default class UserUtils {
+  static async validateUser(_user: IUser) {
+    const user = new User()
+    user.email = _user.email
+    user.name = _user.name
+    user.password = _user.password
+    const errors = await validate(user)
+    if (errors.length === 0) {
+      return user
+    }
+    throw new BadRequestException(errors)
+  }
+}
