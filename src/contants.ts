10ed989ee30dac5b03bd5cefb20954af15997cca
Guilherme dos Santos <guilherme@guisantos.com.br>
2020-08-29 18:21:13 -0300

    Cria constantes

---
+++ src/contants.ts
@@ -0,0 +1,13 @@
+import { MIN } from "class-validator"
+
+export const MIN_LENGTH = {
+  PW: 8,
+  NAME: 5
+}
+
+export const ERRORS_VALIDATION = {
+  STRING: 'Este campo não é uma String',
+  PW_LENGTH: `Tamanho de senha inválido, o mínimo de caracteres são: ${MIN_LENGTH.PW}`,
+  NAME: `Tamanho de nome inválido, o mínimo de caractres são: ${MIN_LENGTH.NAME}`,
+  EMAIL: 'E-mail inválido'
+}
