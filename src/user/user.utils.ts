8d6440b51b5ca96297aa154b6a2c3c2950873d29
Guilherme dos Santos <guilherme@guisantos.com.br>
2020-08-30 23:03:29 -0300

    Separa funcao de auth com de user

--- src/user/user.utils.ts
+++ src/user/user.utils.ts
@@ -1,26 +1,19 @@
 import { validate } from 'class-validator'
-import { hash, compare } from 'bcrypt'
 import IUser from './interfaces/user.interface'
 import { User } from 'entities/user.entity'
 import { BadRequestException } from '@nestjs/common'
 
 export default class UserUtils {
-  static async validateUser(_user: IUser) {
+  static async validateModelUser(_user: IUser) {
     const user = new User()
     user.email = _user.email
     user.name = _user.name
-    user.password = await this.hashPassword(_user.password)
+    user.password = _user.password
     const errors = await validate(user)
     if (errors.length === 0) {
       return user
     }
     throw new BadRequestException(errors)
   }
-  static async hashPassword(password: string) {
-    return await hash(password, 5)
-  }
-
-  static async comparePassword(password: string, encrypted: string) {
-    return await compare(password, encrypted)
-  }
+  
 }
