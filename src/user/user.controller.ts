e1819c56c15d0cbc8540362a92a56b25b891679e
Guilherme dos Santos <guilherme@guisantos.com.br>
2020-08-30 23:05:17 -0300

    Desabilita userController

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,32 +1,25 @@
-import { Controller, Get, HttpCode, Post, Body, Put, Param, Delete } from '@nestjs/common'
-import IUser from './interfaces/user.interface'
-import { UserService } from './user.service'
-import UserUtils from './user.utils'
+/*
 
-@Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) { }
 
-  @Get()
-  @HttpCode(200)
   async getUser(@Body() { email, password } : IUser) {
-    return await this.userService.findUser(email, password)
+    return await this.userService.validateUser(email, password)
   }
 
-  @Post()
   async create(@Body() _user: IUser) {
-    const user = await UserUtils.validateUser(_user)
+    const user = await UserUtils.validateModelUser(_user)
     return this.userService.createUser(user)
   }
-
-  @Put(':email')
+  
   async update(@Param('email') email: string, @Body() _user: IUser) {
-    await UserUtils.validateUser(_user)
+    await UserUtils.validateModelUser(_user)
     return this.userService.updateUser(email, _user)
   }
 
-  @Delete(':email')
+  
   async delete(@Param('email') email: string) {
     return this.userService.deleteUser(email)
   }
 }
+*/
