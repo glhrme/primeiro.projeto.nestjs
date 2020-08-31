b7f73aa67779eb67d49d5397e0345208b66e1b27
Guilherme dos Santos <guilherme@guisantos.com.br>
2020-08-30 23:06:20 -0300

    AuthModule

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,21 @@
+import { AuthService } from './auth.service'
+import { Module } from '@nestjs/common'
+import { PassportModule } from '@nestjs/passport'
+import { UserModule } from 'user/user.module'
+import { LocalStrategy } from './local.strategy'
+import { AuthController } from './auth.controller'
+
+@Module({
+  controllers: [
+    AuthController
+  ],
+  imports: [
+    UserModule,
+    PassportModule
+  ],
+  providers: [
+    AuthService,
+    LocalStrategy
+  ]
+})
+export class AuthModule { }
