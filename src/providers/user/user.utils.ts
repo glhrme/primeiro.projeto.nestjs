f55ea1050f36b853a08b151398f6cf5b528f01f0
Guilherme dos Santos <guilherme@guisantos.com.br>
2020-08-31 01:53:44 -0300

    Hash de senha e tokenValidateEmail

--- src/providers/user/user.utils.ts
+++ src/providers/user/user.utils.ts
@@ -2,13 +2,15 @@ import { validate } from 'class-validator'
 import IUser from './interfaces/user.interface'
 import { User } from '../../database/entities/user.entity'
 import { BadRequestException } from '@nestjs/common'
+import AuthUtils from 'controllers/auth/auth.utils'
 
 export default class UserUtils {
   static async validateModelUser(_user: IUser) {
     const user = new User()
     user.email = _user.email
     user.name = _user.name
-    user.password = _user.password
+    user.password = await AuthUtils.hashPassword(_user.password)
+    user.tokenValidateEmail = Math.random().toString(36)
     const errors = await validate(user)
     if (errors.length === 0) {
       return user
