82d2e658c936fc9083c53888cafd69970298c887
Guilherme dos Santos <guilherme@guisantos.com.br>
2020-08-30 23:04:59 -0300

    Comparador de senhas

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@nestjs/common'
+import { UserService } from 'user/user.service'
+import AuthUtils from './auth.utils'
+
+@Injectable()
+export class AuthService { 
+  constructor(private userService: UserService) { }
+
+  async validateUser(email: string, password: string) {
+    const user = await this.userService.getUser(email)
+    if (user && await AuthUtils.comparePassword(password, user.password)) {
+      delete user.password
+      return user
+    } else {
+      return false
+    }
+  }
+}
