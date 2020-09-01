e625318568b7eaaf350990f3884990852a6d9764
Guilherme dos Santos <guilherme@guisantos.com.br>
2020-09-01 00:34:21 -0300

    Rota de teste Jwt

--- src/controllers/auth/auth.controller.ts
+++ src/controllers/auth/auth.controller.ts
@@ -1,10 +1,12 @@
-import { Controller, UseGuards, Post, Body, Request } from '@nestjs/common'
+import { Controller, UseGuards, Post, Body, Request, Get } from '@nestjs/common'
 import { LocalAuthGuard } from './validations/local-auth.guard'
-import IUser from 'providers/user/interfaces/user.interface'
-import UserUtils from 'providers/user/user.utils'
-import { UserService } from 'providers/user/user.service'
+import IUser from '../../providers/user/interfaces/user.interface'
+import UserUtils from '../../providers/user/user.utils'
+import { UserService } from '../../providers/user/user.service'
 import { AuthService } from './auth.service'
-import { User } from 'database/entities/user.entity'
+import { User } from '../../database/entities/user.entity'
+import { AdminAuthGuard } from './validations/admin-auth.guard'
+import { JwtAuthGuard } from './validations/jwt-auth.guard'
 
 @Controller('auth')
 export class AuthController {
@@ -24,5 +26,11 @@ export class AuthController {
   async signup(@Body() _user: IUser) {
     const user = await UserUtils.validateModelUser(_user)
     return this.userService.createUser(user)
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Post('admin')
+  async admin(@Request() req) {
+    return true
   }
 }
