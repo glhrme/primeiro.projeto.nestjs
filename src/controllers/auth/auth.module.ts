85c376e1c2135f3da275006a1c46c5eb4f1c26ff
Guilherme dos Santos <guilherme@guisantos.com.br>
2020-09-01 00:35:31 -0300

    Adiciona estratégias de login. Devido ao js não processar minhas envs antes desse arquivo, foi necessário adicionar o dotenv neste parte do código também, caso contrario SECRET seria undefined

--- src/controllers/auth/auth.module.ts
+++ src/controllers/auth/auth.module.ts
@@ -1,10 +1,17 @@
-import { AuthService } from './auth.service'
-import { Module } from '@nestjs/common'
-import { PassportModule } from '@nestjs/passport'
 import { UserModule } from 'providers/user/user.module'
-import { LocalStrategy } from './validations/local.strategy'
-import { AuthController } from './auth.controller'
+import { PassportModule } from '@nestjs/passport'
 import { JwtModule } from '@nestjs/jwt'
+import { Module } from '@nestjs/common'
+import { LocalStrategy } from './validations/local.strategy'
+import { JwtStrategy } from './validations/jwt.strategy'
+import { AdminStrategy } from './validations/admin.strategy'
+import { AuthController } from './auth.controller'
+import { AuthService } from './auth.service'
+import * as dotenv from 'dotenv'
+
+dotenv.config({
+  path: process.env.PRODUCTION ? '.env' : '.env.development'
+})
 
 @Module({
   controllers: [
@@ -14,13 +21,16 @@ import { JwtModule } from '@nestjs/jwt'
     UserModule,
     PassportModule,
     JwtModule.register({
-      secret: 'process.env.SECRET_KEY',
+      secret: '' + process.env.SECRET_KEY,
       signOptions: { expiresIn: '7d' }
     })
   ],
   providers: [
     AuthService,
+    JwtStrategy,
+    AdminStrategy,
     LocalStrategy
-  ]
+  ],
+  exports: [AuthService]
 })
 export class AuthModule { }
